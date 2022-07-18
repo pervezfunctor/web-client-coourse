009add1a53940791308c0b5683cc9b43109fa9f7
Pervez Iqbal <pervezfunctor@gmail.com>
2022-07-18 21:40:18 +0530

    minor fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import './index.css'
 import { App } from './App'
 import { createRoot } from 'react-dom/client'
-import './tsDemo'
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
